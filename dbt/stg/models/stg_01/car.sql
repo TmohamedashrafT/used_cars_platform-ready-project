4c6fde3370fa82362743cb82c19904be412d7858
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2024-07-19 05:47:05 +0000

    make edits to the 3NF schema

--- dbt/stg/models/stg_01/car.sql
+++ dbt/stg/models/stg_01/car.sql
@@ -16,25 +16,28 @@ SELECT
     us.mileage,
     us.price,
     us.min_mpg,
-    us.max_mpg
+    us.max_mpg,
+    fu.fuel_id
 FROM 
-		 landing_01.gcs_used_cars us, 
-        {{ ref('engine') }} en,
-        {{ ref('drivetrain') }} dr,
-        {{ ref('color') }} co_in,
-        {{ ref('color') }} co_ex,
-        {{ ref('transmission') }} tr,
-        {{ ref('model') }} mo
+    landing_01.gcs_used_cars us
+JOIN {{ ref('engine') }} en
+    ON (us.engine = en.engine OR (us.engine IS NULL AND en.engine IS NULL)) 
+    AND (us.engine_size = en.engine_size OR (us.engine_size IS NULL AND en.engine_size IS NULL))
+JOIN {{ ref('drivetrain') }} dr
+    ON us.drivetrain = dr.drivetrain OR (us.drivetrain IS NULL AND dr.drivetrain IS NULL)
+JOIN {{ ref('color') }} co_in
+    ON us.interior_color = co_in.color OR (us.interior_color IS NULL AND co_in.color IS NULL)
+JOIN {{ ref('color') }} co_ex
+    ON us.exterior_color = co_ex.color OR (us.exterior_color IS NULL AND co_ex.color IS NULL)
+JOIN {{ ref('transmission') }} tr
+    ON (us.transmission = tr.transmission OR (us.transmission IS NULL AND tr.transmission IS NULL)) 
+    AND (us.automatic_transmission = tr.automatic_transmission OR (us.automatic_transmission IS NULL AND tr.automatic_transmission IS NULL))
+JOIN {{ ref('model') }} mo
+    ON us.model = mo.model OR (us.model IS NULL AND mo.model IS NULL)
+JOIN {{ ref('fuel') }} fu
+    ON us.fuel_type = fu.fuel_type OR (us.fuel_type IS NULL AND fu.fuel_type IS NULL)
 WHERE 
-    ((us.engine = en.engine OR (us.engine IS NULL AND en.engine IS NULL)) AND 
-     (us.engine_size = en.engine_size OR (us.engine_size IS NULL AND en.engine_size IS NULL))) AND
-    (us.drivetrain = dr.drivetrain OR (us.drivetrain IS NULL AND dr.drivetrain IS NULL)) AND
-    (us.interior_color = co_in.color OR (us.interior_color IS NULL AND co_in.color IS NULL)) AND
-    (us.exterior_color = co_ex.color OR (us.exterior_color IS NULL AND co_ex.color IS NULL)) AND
-    (us.transmission = tr.transmission OR (us.transmission IS NULL AND tr.transmission IS NULL)) AND
-    (us.automatic_transmission = tr.automatic_transmission OR (us.automatic_transmission IS NULL AND tr.automatic_transmission IS NULL)) AND
-    (us.model = mo.model OR (us.model IS NULL AND mo.model IS NULL)) AND
-	{{ filter_last_update() }}
+    {{ filter_last_update() }}
 )
 
 select * from car
--- dbt/stg/models/stg_01/model.sql
+++ dbt/stg/models/stg_01/model.sql
@@ -4,13 +4,13 @@
 
 with model as (
     select distinct 
-        dense_rank() over (order by model) as model_id,
+        dense_rank() over (order by uc.model) as model_id,
         uc.model, 
         b.brand_id
-    from `ready-data-de24.landing_01.gcs_used_cars`  uc
-    ,{{ ref('brand') }} b
-    where uc.brand = b.brand or (uc.brand IS NULL AND b.brand IS NULL)
-    and {{ filter_last_update() }}
+    from `ready-data-de24.landing_01.gcs_used_cars` uc
+    join {{ ref('brand') }} b
+        on uc.brand = b.brand or (uc.brand IS NULL AND b.brand IS NULL)
+    where {{ filter_last_update() }}
 )
 
 select * from model
