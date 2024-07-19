b0dd1ec7d60e1159b8b2b2ed0b17ea1c84e86e23
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2024-07-19 05:36:20 +0000

    create and populate dim_car

---
+++ dbt/pl/models/pl_01/dim_car.sql
@@ -0,0 +1,44 @@
+{{ config(
+    materialized='incremental',
+) }}
+
+with car as (
+    select distinct 
+    m.brand_id,
+    COALESCE(b.brand, 'unknown') as brand,
+    c.model_id,
+    COALESCE(m.model, 'unknown') as model,
+    COALESCE(c.year, -1) as year,
+    c.engine_id,
+    COALESCE(e.engine, 'unknown') as engine,
+    COALESCE(e.engine_size, -1) as engine_size,
+    c.transmission_id,
+    COALESCE(t.transmission, 'unknown') as transmission,
+    COALESCE(c.min_mpg, -1) as min_mpg,
+    COALESCE(c.max_mpg, -1) as max_mpg
+
+   from   `ready-data-de24.stg_01.car` c
+   left join stg_01.model m on m.model_id = c.model_id
+   left join stg_01.brand b on b.brand_id = m.brand_id
+   left join stg_01.transmission t on t.transmission_id = c.transmission_id
+   left join stg_01.engine e on e.engine_id = c.engine_id  
+    where not exists(
+    select 1 
+    from pl_01.dim_car ex
+    where 
+    COALESCE(b.brand, 'unknown')  = ex.brand and
+    COALESCE(m.model, 'unknown')  = ex.model and
+    COALESCE(e.engine, 'unknown') =ex.engine and
+    COALESCE(t.transmission, 'unknown') = ex.transmission and
+    COALESCE(c.min_mpg, -1) = ex.min_mpg and
+    COALESCE(c.max_mpg, -1) = ex.max_mpg
+   )
+),
+
+dim_car as (
+
+   select   row_number() OVER() + coalesce((select max(car_key) from pl_01.dim_car), 1) as car_key,
+    *
+   from car    
+)
+select * from dim_car
